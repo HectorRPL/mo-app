5ab8c5ecab6e7970153d505d9959df98c1979e87
HFV <hector.kaizen@gmail.com>
2021-07-22 13:39:22 -0500

    add interfaces

---
+++ projects/mo-links/src/app/interfaces/auth-interface.ts
@@ -0,0 +1,5 @@
+export interface AuthInterface {
+  name: String;
+  email: String;
+  password: String;
+}
---
+++ projects/mo-links/src/app/interfaces/list-interface.ts
@@ -0,0 +1,6 @@
+export interface ListInterface {
+  id?: String;
+  createdAt?: String;
+  url: String;
+  name: String;
+}
